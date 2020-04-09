68c1dbbc6fd47f686ed395530fef1f2ed619a76b
bliaowey <62267175+bliaowey@users.noreply.github.com>
2020-04-09 18:57:19 -0400

    Create Skeptic.java

---
+++ src/lesson19/Skeptic.java
@@ -0,0 +1,12 @@
+public class Skeptic extends Person{
+
+  public Skeptic(){}
+
+  public Skeptic(int x, int y, Country country){
+    super(x, y, country);
+  }
+
+  public void tryToMove(){
+    tryToMoveRandomly();
+  }
+}
