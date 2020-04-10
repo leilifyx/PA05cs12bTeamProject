0a783c63ef24190ce25ff9ce6aa1988df827e5d8
leilifyx <62907855+leilifyx@users.noreply.github.com>
2020-04-09 21:49:18 -0400

    updated MixedPopulation to include radom other people

--- src/lesson19/MixedPopulation.java
+++ src/lesson19/MixedPopulation.java
@@ -1,6 +1,9 @@
 package lesson19;
+import java.util.Random;
+
 
 public class MixedPopulation extends Population{
+	Random random= new Random();
   int numShelterInPlace;
   int numEssential;
   int numOthers;
@@ -20,10 +23,22 @@ public class MixedPopulation extends Population{
     for(int i=0; i<this.numEssential; i++){
       this.addPerson(new StayAtHomeIfSick());
     }
-    /*
+    
     for(int i=0; i<this.numOthers; i++){
-      this.addPerson(new Person());
+      int randInt=random.nextInt(4);//0,1,2,3
+    	if (randInt==0){
+    		this.addPerson(new Skeptic());
+    	}
+    	else if(randInt==1){
+    		this.addPerson(new FrequentFlier());
+    	}
+    	else if(randInt==2){
+    		this.addPerson(new LowImmunityPerson());
+    	}
+    	else if(randInt==3){
+    		this.addPerson(new HighImmunityPerson());
+    	}
     }
-    */
+    
   }
 }
