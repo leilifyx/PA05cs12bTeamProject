5406835c6f05a54ab825abf2444952915e9f3363
Otherfon <liufw2@gmail.com>
2020-04-09 21:57:05 -0400

    Create Immunity_Loss.java

---
+++ src/lesson19/Immunity_Loss.java
@@ -0,0 +1,13 @@
+package lesson19;
+//This class represents people who lose their immunity after a set period of time
+public class Loss extends Person{
+
+  public Loss(){}
+    if recovered && (age - infectionTime - recoveryTime > 5){
+      this.recovered = false;
+    }
+
+
+
+
+}
